8289ef4bb7531c93ab24ec3f0068faf656235fb9
23151911 <23151@burnside.school.nz>
2026-06-02 10:10:01 +1200

    Update

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
-
+from flask import Flask, g, render_template
+import sqlite3
 app = Flask(__name__)
+
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
 
 @app.route("/")
 def home():
-    return "HI"
+    boxers = query_db("SELECT * FROM Boxer")
+    return render_template("Boxing.html", boxers = boxers)
 
 if __name__ == "__main__":
     app.run(debug=True)
